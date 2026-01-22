7d6532c6486303d279b36387076cb29ce76323d9
Changjae Lee <changjae.lee0819@gmail.com>
2026-01-21 21:41:29 -0800

    Update array_string_hashmap.py

--- array_string_hashmap.py
+++ array_string_hashmap.py
@@ -1,3 +1,11 @@
+"""
+- Array
+- String
+- Hashmap
+
+"""
+
+
 # ========================================================
 # Array : 5 questions
 # ========================================================
@@ -6,7 +14,7 @@
 
 class Solution:
     def twoSum(self, nums, target):
-        # Store the original index with each value as a tuple (value, index)
+        
         arr = [(v, i) for i, v in enumerate(nums)]
         arr.sort()
 
@@ -19,3 +27,88 @@ class Solution:
                 l += 1
             else:
                 return [arr[l][1], arr[r][1]]
+            
+# 26. Remove Duplicates from Sorted Array (Easy)
+
+class Solution:
+    def removeDuplicates(self, nums):
+        
+        count = 1
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i-1]:
+                nums[count] = nums[i]
+                count += 1
+                
+        return count
+
+# 121. Best Time to Buy and Sell Stock (Easy)
+
+class Solution:
+    def maxProfit(self, nums):
+        
+        min_price = 10000
+        max_profit = 0
+
+        for price in nums:
+            if price < min_price:
+                min_price = price
+            else:
+                max_profit = max(max_profit, price - min_price)
+        return max_profit
+    
+# 209. Minimum Size Subarray Sum (Medium)
+
+class Solution:
+    def minSubArrayLen(self, nums, target):
+        
+        left = 0
+        current_sum = 0
+        window_min = 10000000
+
+        for right in range(len(nums)):
+            current_sum += nums[right]
+            while target <= current_sum:
+                window_min = min(window_min, right - left + 1)
+                current_sum -= nums[left]
+                left += 1
+
+
+        if window_min ==  10000000:
+            return 0
+        
+        
+        return window_min
+
+# 387. First Unique Character in a String (Easy)
+
+class Solution:
+    def firstUniqChar(self, s):
+        dic_s={}
+        dic_s = {}
+
+        for c in s:
+            if c in dic_s:
+                dic_s[c] += 1
+            else:
+                dic_s[c] = 1
+
+        for i, c in enumerate(s):
+            if dic_s[c] == 1:
+                return i
+
+        return -1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
