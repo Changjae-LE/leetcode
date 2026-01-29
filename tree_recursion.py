865274fad671594fbe3493ed4b25c57ff1401bca
Changjae Lee <changjae.lee0819@gmail.com>
2026-01-29 10:13:45 -0800

    tree_recursion.py

--- tree_recursion.py
+++ tree_recursion.py
@@ -36,3 +36,14 @@ class Solution:
         right = self.maxDepth(root.left)
         left = self.maxDepth(root.right)
         return max(right, left)+1
+
+# 100. Same Tree (Easy)
+
+class Solution:
+    def isSameTree(self, p, q):
+        if p and q:
+            return p.val == q.val and \
+            self.isSameTree(p.left, q.left) and \
+            self.isSameTree(p.right, q.right)
+        
+        return p is q
