00aa04cceaca1735f12c7023c24d8d71a036a530
Changjae Lee <changjae.lee0819@gmail.com>
2026-01-28 19:57:15 -0800

    tree_recursion.py

--- tree_recursion.py
+++ tree_recursion.py
@@ -14,7 +14,7 @@
 
 
 class Solution:
-    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+    def inorderTraversal(self, root):
         rst = []
         self.inorder(root, rst)
         return rst
@@ -27,3 +27,12 @@ class Solution:
         self.inorder(root.right, rst)
         return rst
 
+# 104. Maximum Depth of Binary Tree (Easy)
+
+class Solution:
+    def maxDepth(self, root):
+        if not root:
+            return 0
+        right = self.maxDepth(root.left)
+        left = self.maxDepth(root.right)
+        return max(right, left)+1
