75696a993a01f042a06ba016ecfcd9a9ba6d737b
Changjae Lee <changjae.lee0819@gmail.com>
2026-03-05 12:23:38 -0800

    greedy.py

--- greedy.py
+++ greedy.py
@@ -1,4 +1,6 @@
 # 55. Jump Game (Medium)
+# Time Complexity: O(n)
+# Space Complexity: O(1)
 class Solution:
     def canJump(self, nums):    
         last = len(nums)-1
@@ -21,8 +23,41 @@ class Solution:
                 return True
 
         return True
-# 435. Non-overlapping Intervals
+    
+# 435. Non-overlapping Intervals (Medium)
+# Time Complexity: O(nlogn)
+# Space Complexity: O(1)
 
-# 253. Meeting Rooms II
+class Solution:
+    def eraseOverlapIntervals(self, intervals):#가장 빨리 끝나는거
+        intervals.sort(key=lambda x: x[1])
+        removed = 0
+        prev_end = intervals[0][1]
+        for s, e in intervals[1:]:
+            if s < prev_end:
+                removed += 1
+            else:
+                prev_end = e
 
-# 134.
+        return removed
+# 253. Meeting Rooms II (Medium)
+# Time Complexity: O(nlogn)
+# Space Complexity: O(n)
+
+class Solution:
+    def minMeetingRooms(self, intervals):
+        starts = sorted(i[0] for i in intervals)
+        ends = sorted(i[1] for i in intervals)
+
+        rooms = 0
+        end_ptr = 0
+
+        for s in starts:
+            if s < ends[end_ptr]:
+                rooms += 1
+            else:
+                end_ptr += 1
+
+        return rooms
+
+# 134. Gas Station (Medium)
