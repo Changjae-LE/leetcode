3713af2d6d4ed07de43e37b750a3b80d756b3f27
Changjae Lee <changjae.lee0819@gmail.com>
2026-05-19 19:23:37 -0700

    medium.py

--- medium.py
+++ medium.py
@@ -39,3 +39,35 @@ class Solution:
                     right -= 1
 
         return rst
+
+
+# ======================================================================
+# 79. Word Search
+# Topic : DFS, Backtracking
+# ======================================================================
+# Time Complexity: O(n^2), Space Complexity: O(1)
+class Solution:
+    def exist(self, board, word):
+
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if self.dfs(board, word, i, j, 0):
+                    return True
+        return False
+
+    def dfs(self, board, word, i, j, idx):
+        if i < 0 or i == len(board) or j < 0 or j == len(board[0]):
+            return False
+        if board[i][j] != word[idx] or board[i][j] == '*':
+            return False
+        if idx == len(word) - 1:
+            return True
+        
+        cache = board[i][j]
+        board[i][j] = '*'
+        rst = self.dfs(board, word, i+1, j, idx+1) or \
+            self.dfs(board, word, i-1, j, idx+1) or \
+            self.dfs(board, word, i, j+1, idx+1) or \
+            self.dfs(board, word, i, j-1, idx+1)
+        board[i][j] = cache
+        return rst
