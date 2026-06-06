4b28e675803469e8aa379bfc3b79a1f78be0f0eb
Changjae Lee <changjae.lee0819@gmail.com>
2026-06-05 17:34:40 -0700

    medium.py

--- medium.py
+++ medium.py
@@ -391,4 +391,32 @@ class Solution:
             max_len = max(max_len, right-left+1)
             last[s[right]] = right+1
 
-        return int(max_len)
+        return max_len
+
+# ======================================================================
+# 5. Longest Palindromic Substring
+# Topic : Two Pointers
+# ======================================================================
+# Time Complexity: O(), Space Complexity: O()
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        ans = ""
+
+        for i in range(len(s)):
+            odd = self.expand(s, i, i)
+            even = self.expand(s, i, i + 1)
+
+            if len(odd) > len(ans):
+                ans = odd
+
+            if len(even) > len(ans):
+                ans = even
+
+        return ans
+
+    def expand(self, s, left, right):
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            left -= 1
+            right += 1
+
+        return s[left + 1:right]
